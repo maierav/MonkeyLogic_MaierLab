ffbff437bdcd647c859169b0a23e65367f9b211a
micheleacox <michele.a.cox@gmail.com>
2014-07-24 18:51:26 -0500

    further RNS dev

--- DEV/rns/gMovieRNS.m
+++ DEV/rns/gMovieRNS.m
@@ -1,14 +1,24 @@
-% X x Y x 4 x N object to function as a Movie (equivalent to "Mov") with N frames.
+function [rns x y moreinfo] = gMovieRNS(TrialRecord)
 
-function [rns x y] = gMovieRNS(TrialRecord)
+% random noise stimuli for rf mapping
+% July 2014
+% MAC
+
+framen = 10;
+moviestep = 0.5;
+moviewraps = 1;
+
+frameln   = TrialRecord.ScreenInfo.FrameLength; % ms/frame, actually measured by ML
+movie_play_dur  = frameln/moviestep * framen * moviewraps;  %msperframe * numberofframes * numberofrepeats  
 
 pixperdeg = TrialRecord.ScreenInfo.PixelsPerDegree;
-xdegrees = TrialRecord.ScreenInfo.Xdegrees/2;
-ydegrees = TrialRecord.ScreenInfo.Ydegrees/2;
+xdegrees  = 6;
+ydegrees  = 6;
+
 xpix = floor(xdegrees * pixperdeg);
 ypix = floor(ydegrees * pixperdeg);
 
-rns = zeros(xpix,ypix,4,5);
+rns = zeros(xpix,ypix,framen,10);
 channel = rns(:,:,1,:);
 color = randi([1  numel(channel)],[1 numel(channel)/2]);
 channel(color) = 1;
@@ -16,10 +26,38 @@ for i=1:4
     rns(:,:,i,:) = channel;
 end
 
-x = floor(xdegrees/2 -2);
-y = floor(ydegrees/2 - 2);
+x = 2.5;
+y = -2.5;
 
+% save each trials RNS
+[fpath fname] = fileparts(TrialRecord.BhvFileName);
+newdir = [fpath filesep 'rns'];
+if exist(newdir,'dir') ~= 7
+    mkdir(newdir)
+end
+filename = fullfile(newdir,sprintf('%s-tr%03d.rns',fname,TrialRecord.CurrentTrialNumber));
+fid = fopen(filename, 'w');
+fwrite(fid,rns,'uint8');
+fclose(fid);
+% save rns info
+if TrialRecord.CurrentTrialNumber == 1
+    rnsinfo.size = size(rns); % pixels
+    rnsinfo.x_pos    = x; % dva
+    rnsinfo.y_pos    = y; % dva
+    rnsinfo.x_ln     = xdegrees; % dva
+    rnsinfo.y_ln     = ydegrees; % dva
+    rnsinfo.n   = framen;
+    rnsinfo.moviestep     = moviestep;
+    rnsinfo.n_movie_wraps = moviewraps;
+    rnsinfo.ms_per_frame      =  frameln/moviestep;
+    rnsinfo.movie_play_dur    =  movie_play_dur;
+    filename = fullfile(newdir,sprintf('%s-rnsinfo.mat',fname));
+    save(filename,'rnsinfo','-mat');
+end
+    
+% pass params timing info to timing file
+moreinfo = [moviestep movie_play_dur];
 
-TrialRecord.MovieStartFrame = 1;
-TrialRecord.MovieStep = 0.5;
+end
+
 
---
+++ DEV/rns/tRNS.m
@@ -0,0 +1,69 @@
+% July 2014
+% MAC
+
+% set editable vars
+editable({'fix_radius','wait_for_fix','fix_dur','punish_dur','iti_dur'});
+
+% give names to the TaskObjects defined in the conditions file:
+fix  = 1;
+mov  = 2;
+
+% fixation window (in degrees):
+fix_radius = 10;
+
+% define time intervals (in ms):
+wait_for_fix = 1000;
+fix_dur = 100;
+movie_dur = TrialRecord.CurrentConditionStimulusInfo{mov}.MoreInfo(2);
+punish_dur = 2000;
+iti_dur = 1000; 
+
+
+% set ITI, "The desired duration can be reset to the value from the main menu by calling set_iti with duration == -1"
+set_iti(iti_dur);
+
+%TASK:
+
+% send block and condition number
+eventmarker(116 + TrialRecord.CurrentBlock)
+eventmarker(116 + TrialRecord.CurrentCondition)
+
+% initial fixation:
+toggleobject(fix,'EventMarker',35);
+ontarget = eyejoytrack('acquirefix', fix, fix_radius, wait_for_fix);
+if ~ontarget,
+    trialerror(4); % no fixation
+    toggleobject(fix,'EventMarker',36);
+    idle(punish_dur); % punishment delay
+    return
+end
+
+% hold fixation
+ontarget = eyejoytrack('holdfix', fix, fix_radius, fix_dur);
+if ~ontarget,
+    trialerror(3); % broke fixation
+    toggleobject(fix,'EventMarker',36); 
+    idle(punish_dur) % punishment delay
+    return
+end
+
+% show mov
+moviestep = TrialRecord.CurrentConditionStimulusInfo{mov}.MoreInfo(1);
+toggleobject(mov,'MovieStep',moviestep,'EventMarker',23)
+ontarget = eyejoytrack('holdfix', fix, fix_radius, movie_dur);
+if ~ontarget,
+    trialerror(3); % broke fixation
+    toggleobject(mov,'EventMarker',24)
+    idle(punish_dur); % punishment delay
+    return
+end
+toggleobject(mov,'EventMarker',24)
+
+% all objects off
+trialerror(0); % correct
+toggleobject([fix mov],'status','off','EventMarker',36);
+
+
+
+
+
